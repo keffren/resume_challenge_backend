b30c7c5f8cc8770f720a2aecdb8d68911a0cee00
keffren <sernamateouk@gmail.com>
2023-11-18 20:29:45 +0000

    Validate CodePipeLine's deploy stage

--- getVisitorsCount.py
+++ getVisitorsCount.py
@@ -3,14 +3,14 @@ import boto3
 
 def getVisitorsCount_handler(event, context):
     
-    #Connect to DynamoDB and retrieve the item
+    # Connect to DynamoDB and retrieve the item
     dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
     
     table_name = "resume-challenge-counter"
     table = dynamodb.Table(table_name)
     item = table.get_item(Key={"CounterId": "mainCounter"})
     
-    #Get the counter value
+    # Get the counter value
     counter_value = int(item["Item"]["visitorsNumber"])
 
     # Validate CodePipeLine's deploy stage 
